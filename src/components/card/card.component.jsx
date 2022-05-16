f3a65050c6d4ab2c9fe1809edec14fc631510758
Chitransh Singhal <singhalchitransh01@gmail.com>
2022-05-16 18:46:58 +0530

    search functionality

--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -2,8 +2,8 @@ import React from "react";
 
 import "./card.styles.css";
 
-const Card = (props) => {
-  const { id, name, email } = props.monster;
+const Card = ({ monster }) => {
+  const { id, name, email } = monster;
 
   return (
     <>
